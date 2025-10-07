3a6ade2beaa58b217f395f56b6f576cea6cae115
ibrahim dursun <ibrahim@dursun.cc>
2025-10-07 22:24:47 +0100

    feat: allow binding multiple key presses to one action

--- internal/ui/actions/action_map.go
+++ internal/ui/actions/action_map.go
@@ -1,5 +1,11 @@
 package actions
 
+import (
+	"strings"
+
+	"github.com/charmbracelet/lipgloss"
+)
+
 type ActionBinding struct {
 	On []string `toml:"on"`
 	Do Action   `toml:"do"`
@@ -46,7 +52,7 @@ func (am *ActionMap) UnmarshalTOML(data any) error {
 	return nil
 }
 
-func (am ActionMap) Get(key string) (*ActionBinding, bool) {
+func (am *ActionMap) Get(key string) (*ActionBinding, bool) {
 	for i := range am.Bindings {
 		binding := &am.Bindings[i]
 		for _, on := range binding.On {
@@ -56,4 +62,39 @@ func (am ActionMap) Get(key string) (*ActionBinding, bool) {
 		}
 	}
 	return nil, false
+}
+
+func (am *ActionMap) GetMatch(previous []string, key string) []*ActionBinding {
+	var matches []*ActionBinding
+	for i := range am.Bindings {
+		binding := &am.Bindings[i]
+		if len(binding.On) <= len(previous) {
+			continue
+		}
+		for i := 0; i < len(previous); i++ {
+			if binding.On[i] != previous[i] {
+				goto next
+			}
+		}
+		if binding.On[len(previous)] != key {
+			goto next
+		}
+		matches = append(matches, binding)
+	next:
+	}
+	return matches
+}
+
+var border = lipgloss.NewStyle().Border(lipgloss.NormalBorder()).PaddingLeft(1).PaddingRight(1)
+
+// RenderAvailableActions renders the available actions
+// This is temporary until a better UI is implemented
+func RenderAvailableActions(bindings []*ActionBinding, textStyle lipgloss.Style, shortCutStyle lipgloss.Style) string {
+	lines := make([]string, len(bindings))
+	for i, binding := range bindings {
+		keys := strings.Join(binding.On, " ")
+		lines[i] = lipgloss.JoinHorizontal(0, shortCutStyle.Render(keys), textStyle.Render(" ", binding.Do.GetDesc()))
+	}
+	content := lipgloss.JoinVertical(0, lines...)
+	return border.Render(content)
 }
